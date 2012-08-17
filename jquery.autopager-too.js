e370b05a27744138ecb685641a1643b382eccd22
Cristina Hanes <chanes@Cristinas-iMac.local>
2012-08-17 12:13:37 -0700

    Update description

--- jquery.autopager-too.js
+++ jquery.autopager-too.js
@@ -1,8 +1,18 @@
 /*
- * jQuery.autopager v1.0.0
+ * jQuery.autopager Too v1.0.1
  *
- * Copyright (c) lagos
+ * Copyright (c) cristinawithout
+ * Based on jQuery.autopager copyright (c) Lagos
+ * 
  * Dual licensed under the MIT and GPL licenses.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE. 
  */
 (function($) {
   var window = this, options = {},
